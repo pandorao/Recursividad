2e4c040d6924cca6d9c68f486915af55ed729f27
Javier Roncallo <jersy96@hotmail.com>
2016-11-03 10:25:20 -0500

    temporal, solo mientras decido si elimino la clase Graph o no

--- src/logic/Graph.java
+++ src/logic/Graph.java
@@ -5,12 +5,14 @@ public class Graph {
     int vertices, edges;
     boolean[][] adj;
     int[][] vertexMatrix;
+    int[][] visited;
+    int contVisited;
+    String toVisit;
 
     public Graph(Maze maze) {
         int contEdges = 0;
-        this.vertices = maze.vertexCount();
+        this.enumerateVertexMatrix(maze);
         adj = new boolean[vertices + 1][vertices + 1];
-        enumerateVertexMatrix(maze);
         int[][] matrix = maze.getMatrix();
         for (int i = 0; i < maze.filas; i++) {
             for (int j = 0; j < maze.columnas; j++) {
@@ -28,19 +30,8 @@ public class Graph {
         this.edges = contEdges;
     }
 
-    public final void addEdge(int a, int b) {
+    private void addEdge(int a, int b) {
         adj[a][b] = adj[b][a] = true;
-    }
-
-    boolean[] visited;
-
-    public void DFS(int v) {
-        visited[v] = true;
-        for (int i = 1; i <= vertices; i++) {
-            if (adj[v][i] && !visited[i]) {
-                DFS(i);
-            }
-        }
     }
 
     private void enumerateVertexMatrix(Maze maze) {
@@ -56,6 +47,44 @@ public class Graph {
                     vertexMatrix[i][j] = 0;
                 }
                 System.out.print(vertexMatrix[i][j] + "     ");
+            }
+        }
+        vertices = cont;
+    }
+
+    private void newToVisit(int newToVisit) {
+        if (toVisit.equals("")) {
+            toVisit = "" + newToVisit;
+        } else {
+            toVisit = "-" + newToVisit;
+        }
+    }
+
+    private int getLastToVisitAndRemove() {
+        int len = toVisit.length();
+        String s = toVisit.substring(len - 1);
+        int ret = Integer.parseInt(s);
+        toVisit = toVisit.substring(0, len - 1);
+        return ret;
+    }
+
+    public int[] getShortestPath(int vInit, int vMeta) {
+        visited = new int[vertices + 1][2];
+        toVisit = "";
+        contVisited = 0;
+        newToVisit(vInit);
+        while (!toVisit.equals("")) {
+            int actual = getLastToVisitAndRemove();
+            for (int i = 1; i <= vertices; ++i) {
+                if (adj[actual][i] && !visited[i]) {
+                    visited[contVisited][0] = i;
+                    visited[contVisited][1] = actual;
+                    if(i == vMeta){
+                        toVisit = "";
+                    }else{
+                        newToVisit(i);
+                    }
+                }
             }
         }
     }
