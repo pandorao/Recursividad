ec7d181d2df60aee03193a0ea7aa072cfcf29b9c
Juan D Lagares <juandavidlagareas@gmail.com>
2016-11-09 10:58:21 -0500

    Ahora al inicio de del programa al presionar cancelar se cierra inmediatamente, pues antes no lo hacia

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -7,23 +7,29 @@ public class Main {
 
     public static void main(String[] args) {
         String filas_s, columnas_s, bloqueados_s;
-        int s, filas=0, columnas=0, bloqueados=0;
+        int s, filas = 0, columnas = 0, bloqueados = 0;
         do {
             filas_s = JOptionPane.showInputDialog(null, "Escriba el numero de filas", "Configuracion", JOptionPane.PLAIN_MESSAGE);
-            columnas_s = JOptionPane.showInputDialog(null, "Escriba el numero de columnas", "Configuracion", JOptionPane.PLAIN_MESSAGE);
-            bloqueados_s = JOptionPane.showInputDialog(null, "Escriba el numero de bloques bloqueados", "Configuracion", JOptionPane.PLAIN_MESSAGE);
-            s = ReceivedDataValidator.ValidateInput(filas_s, columnas_s, bloqueados_s);
-            if (filas_s == null || columnas_s == null || bloqueados_s==null) {
+            if (filas_s == null) {
                 System.exit(0);
             }
-            if (s!=ReceivedDataValidator.VALIDATION_SUCCES) {
-                JOptionPane.showMessageDialog(null, ReceivedDataValidator.getErrorDescription(s), "Error",JOptionPane.ERROR_MESSAGE);
-            } else {
-                filas=Integer.parseInt(filas_s);
-                columnas=Integer.parseInt(columnas_s);
-                bloqueados=Integer.parseInt(bloqueados_s);             
+            columnas_s = JOptionPane.showInputDialog(null, "Escriba el numero de columnas", "Configuracion", JOptionPane.PLAIN_MESSAGE);
+            if (columnas_s == null) {
+                System.exit(0);
             }
-        } while (ReceivedDataValidator.VALIDATION_SUCCES!=s);
+            bloqueados_s = JOptionPane.showInputDialog(null, "Escriba el numero de bloques bloqueados", "Configuracion", JOptionPane.PLAIN_MESSAGE);
+            if (bloqueados_s == null) {
+                System.exit(0);
+            }
+            s = ReceivedDataValidator.ValidateInput(filas_s, columnas_s, bloqueados_s);
+            if (s != ReceivedDataValidator.VALIDATION_SUCCES) {
+                JOptionPane.showMessageDialog(null, ReceivedDataValidator.getErrorDescription(s), "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                filas = Integer.parseInt(filas_s);
+                columnas = Integer.parseInt(columnas_s);
+                bloqueados = Integer.parseInt(bloqueados_s);
+            }
+        } while (ReceivedDataValidator.VALIDATION_SUCCES != s);
         Maze maze = new Maze(filas, columnas, bloqueados);
         MainFrame mf = new MainFrame(maze);
         mf.setVisible(true);
